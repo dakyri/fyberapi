b0cd2539a54e5c42bc61b41afe0de36d334b93a7
David Karla <dak@mayaswell.com>
2016-05-15 21:00:18 +0200

    added instrumentation test

--- app/src/androidTest/java/com/mayaswell/fyberapi/FyberAPITest.java
+++ app/src/androidTest/java/com/mayaswell/fyberapi/FyberAPITest.java
@@ -1,29 +1,34 @@
 package com.mayaswell.fyberapi;
 
+import android.content.Context;
 import android.net.Uri;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.AndroidTestCase;
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by dak on 5/15/2016.
+ *
+ * unit test for things not mockable
  */
-public class FyberAPITest extends AndroidTestCase {
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class FyberAPITest extends InstrumentationTestCase {
 
 	private FyberAPI fyberAPI;
 
 	@Before
 	public void setUp() throws Exception {
-		fyberAPI = new FyberAPI(getContext(), getContext().getString(R.string.base_url), false);
+		Context c = getInstrumentation().getContext();
+		fyberAPI = new FyberAPI(c, c.getString(R.string.base_url), false);
 		fyberAPI.setGooApis("a1e5b437-0259-4f20-a16c-b6cc838a37c5", false);
-	}
-
-	@Test
-	public void testVerifyResponsHash() throws Exception {
-
 	}
 
 	@Test
